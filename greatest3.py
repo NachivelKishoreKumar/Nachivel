717ccf5078d90f50967cc599e7864af4dc2492b7
NachivelKishoreKumar <53596289+NachivelKishoreKumar@users.noreply.github.com>
2019-08-07 15:45:45 +0530

    Add files via upload

---
+++ greatest3.py
@@ -0,0 +1,9 @@
+a=int(input("enter value:"))
+b=int(input("enter value:"))
+c=int(input("enter value:"))
+if a>b and a>c:
+    print("a is greater")
+elif b>a and b>c:
+    print("b is greater")
+else:
+    print("c is greater")
