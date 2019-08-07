ffdc77e7aaec7f2fc3893a66bdba5927ba7cae82
NachivelKishoreKumar <53596289+NachivelKishoreKumar@users.noreply.github.com>
2019-08-07 22:23:22 +0530

    Create Digitslet.py

---
+++ Digitslet.py
@@ -0,0 +1,11 @@
+str1=input("Enter string:")
+Digits=0
+Letters=0
+for i in string:
+      if(i.isdigit()):
+            Digits=Digits+1
+      Letter=Letter+1
+print("The number of digits is:")
+print(Digits)
+print("The number of characters is:")
+print(Letter)
