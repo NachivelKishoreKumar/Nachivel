1f98ba53cc70d7920c9005e89619cca156500c5b
NachivelKishoreKumar <53596289+NachivelKishoreKumar@users.noreply.github.com>
2019-08-08 06:35:53 +0530

    Update Digitslet.py

--- Digitslet.py
+++ Digitslet.py
@@ -1,11 +1,11 @@
 str1=input("Enter string:")
 Digits=0
 Letters=0
-for i in string:
+for i in str1:
       if(i.isdigit()):
             Digits=Digits+1
-      Letter=Letter+1
+      Letters=Letters+1
 print("The number of digits is:")
 print(Digits)
-print("The number of characters is:")
-print(Letter)
+print("The number of Letters is:")
+print(Letters)
