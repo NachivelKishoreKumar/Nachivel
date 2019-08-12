9abb4fc8cad1d1cccc5420b1f966685d80b67db2
NachivelKishoreKumar <53596289+NachivelKishoreKumar@users.noreply.github.com>
2019-08-12 19:31:03 +0530

    Create arearect.py

---
+++ arearect.py
@@ -0,0 +1,8 @@
+class rectangle():
+           def __init__(self,l,b):
+                 self.l=l
+                 self.b=b
+           def areaof(self):
+                 print (self.l*self.b)
+rect=rectangle(5,6)
+rect.areaof()
