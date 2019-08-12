50afc02907e75ca0db91fcfd9b4524d9bc86d5d4
NachivelKishoreKumar <53596289+NachivelKishoreKumar@users.noreply.github.com>
2019-08-12 19:42:27 +0530

    Rename areacircle.py to Circle.py

---
+++ Circle.py
@@ -0,0 +1,10 @@
+class circle():
+           def __init__(self,r):
+                 self.r=r
+           def areaof(self):
+                 print(3.14*(self.r**2))
+           def periof(self):
+                 print(2*3.14*self.r)
+cir=circle(7)
+cir.areaof()
+cir.periof()
